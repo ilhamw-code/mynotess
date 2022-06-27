227fce55044296f94faf43140dcb5a025a21ead5
wahyu-maker <wahyuilham20@gmail.com>
2022-06-27 11:30:20 +0700

    step-11

---
+++ lib/services/crud/note_service.dart
@@ -0,0 +1,268 @@
+import 'package:flutter/cupertino.dart';
+import 'package:note/services/crud/crud_exception.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' show join;
+
+class NotesServices {
+  Database? _db;
+
+  Future<DatabaseNote> updateNote({
+    required DatabaseNote note,
+    required String text,
+  }) async {
+    final db = _getDatabaseOrThrow();
+    await getNote(id: note.id);
+
+    final updateCount = await db.update(noteTable, {
+      textColumn: text,
+      isSyncedWithCloudColumn: 0,
+    });
+    if (updateCount == 0) {
+      throw CouldNotUpdateNote();
+    } else {
+      return await getNote(id: note.id);
+    }
+  }
+
+  Future<Iterable<DatabaseNote>> getAllNote() async {
+    final db = _getDatabaseOrThrow();
+    final notes = await db.query(noteTable);
+
+    return notes.map((noteRow) => DatabaseNote.fromRow(noteRow));
+  }
+
+  Future<DatabaseNote> getNote({required int id}) async {
+    final db = _getDatabaseOrThrow();
+    final notes = await db.query(
+      noteTable,
+      limit: 1,
+      where: 'id =?',
+      whereArgs: [id],
+    );
+    if (notes.isEmpty) {
+      throw CouldNotFindNote();
+    } else {
+      return DatabaseNote.fromRow(notes.first);
+    }
+  }
+
+  Future<int> deleteAllNote() async {
+    final db = _getDatabaseOrThrow();
+    return await db.delete(noteTable);
+  }
+
+  Future<void> deleteNote({required int id}) async {
+    final db = _getDatabaseOrThrow();
+
+    final deletedCount = await db.delete(
+      noteTable,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    if (deletedCount == 0) {
+      throw CouldNotDeleteNote();
+    }
+  }
+
+  Future<DatabaseNote> createNote({required DatabaseUser owner}) async {
+    final db = _getDatabaseOrThrow();
+
+    // make sure owner exists in the database with the correct id
+    final dbUser = await getUser(email: owner.email);
+    if (dbUser != owner) {
+      throw CouldNotfindUser();
+    }
+    const text = '';
+    // create the note
+    final noteId = await db.insert(noteTable, {
+      userIdColumn: owner.id,
+      textColumn: text,
+      isSyncedWithCloudColumn: 1,
+    });
+
+    final note = DatabaseNote(
+      id: noteId,
+      userId: owner.id,
+      text: text,
+      isSyncWithCloud: true,
+    );
+    return note;
+  }
+
+  Future<DatabaseUser> getUser({required String email}) async {
+    final db = _getDatabaseOrThrow();
+
+    final results = await db.query(
+      userTable,
+      limit: 1,
+      where: 'email =?',
+      whereArgs: [email.toLowerCase()],
+    );
+
+    if (results.isEmpty) {
+      throw CouldNotfindUser();
+    } else {
+      // return data dari DatabaseUser yang paling awal
+      return DatabaseUser.fromRow(results.first);
+    }
+  }
+
+  Future<DatabaseUser> createUser({required String email}) async {
+    final db = _getDatabaseOrThrow();
+    final results = await db.query(
+      userTable,
+      limit: 1,
+      where: 'email =?',
+      whereArgs: [email.toLowerCase()],
+    );
+    if (results.isNotEmpty) {
+      throw UserAlreadyExists();
+    }
+
+    final userId = await db.insert(userTable, {
+      emailColumn: email.toLowerCase(),
+    });
+
+    return DatabaseUser(id: userId, email: email);
+  }
+
+  Future<void> deleteUser({required String email}) async {
+    final db = _getDatabaseOrThrow();
+
+    final deletedCount = await db.delete(
+      userTable,
+      where: 'email =?',
+      whereArgs: [email.toLowerCase()],
+    );
+    if (deletedCount != 1) {
+      throw CouldNotDeleteUser();
+    }
+  }
+
+  Database _getDatabaseOrThrow() {
+    final db = _db;
+    if (db == null) {
+      throw DatabaseIsNotOpen();
+    } else {
+      return db;
+    }
+    // gunanya supaya kita tidak mengulang coding lagi dan bisa digunakan secara instans
+  }
+
+  Future<void> close() async {
+    final db = _db;
+
+    if (db == null) {
+      throw DatabaseIsNotOpen();
+    } else {
+      await db.close();
+      _db == null; // ketika close kita juga harus me reset db menjadi null
+    }
+  }
+
+  Future<void> open() async {
+    if (_db != null) {
+      throw DatabaseAlreadyOpenException();
+    }
+    try {
+      final docsPath =
+          await getApplicationDocumentsDirectory(); // get document dari aplikasi
+      final dbPath = join(docsPath.path, dbName); // ini akan mengambil dbnote
+      final db = await openDatabase(dbPath);
+      _db = db;
+
+      // create the user table
+
+      await db.execute(createUserTable);
+
+      //create the note table
+
+      await db.execute(createNoteTable);
+    } on MissingPlatformDirectoryException {
+      throw UnableToGetDocumentsDirectory();
+    }
+  }
+}
+
+@immutable
+class DatabaseUser {
+  final int id;
+  final String email;
+  // sesuaikan field dengan db browser yang telah dibuat
+  const DatabaseUser({
+    required this.id,
+    required this.email,
+  });
+
+  DatabaseUser.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        email = map[emailColumn] as String;
+
+  @override
+  // ini untuk menampiilkan nya ke konsol
+  String toString() => 'Person, ID = $id, email = $email ';
+
+  @override
+  // ini untuk cek id, apakah dengan id yang sama atau id berbeda
+  bool operator ==(covariant DatabaseUser other) => id == other.id;
+
+  @override
+  // harus override hash untukcek id yang sama di atas
+  int get hashCode => id.hashCode;
+}
+
+class DatabaseNote {
+  final int id;
+  final int userId;
+  final String text;
+  final bool isSyncWithCloud;
+
+  DatabaseNote({
+    required this.id,
+    required this.userId,
+    required this.text,
+    required this.isSyncWithCloud,
+  });
+  DatabaseNote.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        userId = map[userIdColumn] as int,
+        text = map[textColumn] as String,
+        isSyncWithCloud =
+            (map[isSyncedWithCloudColumn] as int) == 1 ? true : false;
+  // Sync ini bertipe bool jadi lansung kita eksekusi dengan true atau false;
+
+  @override
+  String toString() =>
+      ' Note, ID = $id, userID = $userId, text = $text, isSyncWithCloud = $isSyncWithCloud';
+
+  @override
+  // ini untuk cek id, apakah dengan id yang sama atau id berbeda
+  bool operator ==(covariant DatabaseUser other) => id == other.id;
+
+  @override
+  // harus override hash untukcek id yang sama di atas
+  int get hashCode => id.hashCode;
+}
+
+const dbName = 'note.db'; // ini adalah nama file database yang di DB browser
+const noteTable = 'note';
+const userTable = 'user';
+const idColumn = 'id';
+const emailColumn = 'email';
+const userIdColumn = 'user_id';
+const textColumn = 'text';
+const isSyncedWithCloudColumn = 'is_sync_with_cloud';
+const createUserTable = '''CREATE TABLE IF NOT EXISTS "user" (
+            "id"	INTEGER NOT NULL,
+            "email"	TEXT NOT NULL UNIQUE,
+            PRIMARY KEY("id" AUTOINCREMENT)
+          );''';
+const createNoteTable = ''' CREATE TABLE IF NOT EXISTS "note" (
+        "id"	INTEGER NOT NULL,
+        "user_id"	INTEGER NOT NULL,
+        "text"	TEXT,
+        "is_synced_with_cloud"	INTEGER NOT NULL DEFAULT 0,
+        FOREIGN KEY("user_id") REFERENCES "user"("id"),
+        PRIMARY KEY("id" AUTOINCREMENT)
+      ); ''';
